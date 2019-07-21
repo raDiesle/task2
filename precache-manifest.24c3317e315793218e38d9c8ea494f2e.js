0f78006ecb8f9482e6369796dbada482e4ca97e5
damend <amend@crxmarkets.com>
2019-07-21 11:01:21 +0200

    Updates

---
+++ precache-manifest.24c3317e315793218e38d9c8ea494f2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6a05a9ad29cbaf3de92be09afbad8a",
+    "url": "/raDiesle/task2/index.html"
+  },
+  {
+    "revision": "c8b4332b2652b2434a9c",
+    "url": "/raDiesle/task2/static/css/main.805b6c8e.chunk.css"
+  },
+  {
+    "revision": "01c4096dd90cd4c1448a",
+    "url": "/raDiesle/task2/static/js/2.21469991.chunk.js"
+  },
+  {
+    "revision": "c8b4332b2652b2434a9c",
+    "url": "/raDiesle/task2/static/js/main.c2fe3308.chunk.js"
+  },
+  {
+    "revision": "d9808c794bb4f3520986",
+    "url": "/raDiesle/task2/static/js/runtime~main.48541fb6.js"
+  }
+]);
